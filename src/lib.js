acdab5da2d2f0a3a48f6d798b6017e3dca8b2b61
Jeremy Krovitz <jkrovitz@JeremyWliamComp.hitronhub.home>
2021-10-30 21:59:37 -0500

    Add code for assignment

--- src/lib.js
+++ src/lib.js
@@ -1,25 +1,73 @@
 import fs from 'fs'
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
+const _ = require("lodash");
+
+export const chooseRandom = (array, num_items) =>
+    array.length === 0 || array.length === 1 ?
+        array : num_items === null ?
+        _.sample(array) : _.sampleSize(array, num_items)
+
+export const createPrompt = ({numQuestions = 1, numChoices = 2} = {}) => {
+    const questionObj = questionNumber => ({
+        type: 'input',
+        name: `question-${questionNumber}`,
+        message: `Enter question ${questionNumber}`
+    })
+
+    const choiceObj = (choiceNumber, questionNumber) => ({
+        type: 'input',
+        name: `question-${questionNumber}-choice-${choiceNumber}`,
+        message: `Enter answer choice ${choiceNumber} for question ${questionNumber}`
+    })
+
+    return Array(numQuestions + numQuestions * numChoices)
+        .fill()
+        .map((val, idx) => {
+            if (idx % (numChoices + 1) === 0) {
+                return questionObj(
+                    idx / (numChoices + 1) ? idx / (numChoices + 1) : 1
+                )
+            } else {
+                return choiceObj(
+                    idx % (numChoices + 1),
+                    Math.ceil(idx / (numChoices + 1))
+                )
+            }
+        })
 }
 
-export const createPrompt = () => {
-  // TODO implement createPrompt
-}
+export const createQuestions = (questionObject = {}) => {
+    let questionKeys = Object.keys(questionObject)
 
-export const createQuestions = () => {
-  // TODO implement createQuestions
+    let questions = {}
+
+    questionKeys.forEach(element => {
+        if (!element.includes('choice')) {
+            questions[element] = {
+                type: 'list',
+                name: element,
+                message: questionObject[element],
+                choices: []
+            }
+        } else {
+            let indexString = 'question-' + element.split('-')[1]
+            let tempObj = questions[indexString]
+            tempObj['choices'].push(questionObject[element])
+        }
+        return Object.values(questions)
+    })
+
+    return Object.values(questions)
 }
 
 export const readFile = path =>
-  new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => (err ? reject(err) : resolve(data)))
-  })
+    new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => (err ? reject(err) : resolve(data)))
+    })
 
 export const writeFile = (path, data) =>
-  new Promise((resolve, reject) => {
-    fs.writeFile(path, data, err =>
-      err ? reject(err) : resolve('File saved successfully')
-    )
-  })
+    new Promise((resolve, reject) => {
+        fs.writeFile(path, data, err =>
+            err ? reject(err) : resolve('File saved successfully')
+        )
+    })
